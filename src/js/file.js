eeb4ec947593eb9f4c552936d6875d278d8ca92d
محمد سمير سعد يوسف علي <132437456+abosamraaa@users.noreply.github.com>
2023-10-19 19:31:20 +0300

    Update file.js

--- src/js/file.js
+++ src/js/file.js
@@ -6,23 +6,23 @@ let iphone5 = document.querySelector('#red-image');
 let mainImage = document.querySelector('.main-image')
 function red(){
     document.body.style.background = "#c82525";
-    mainImage.innerHTML = `<img src="/images/4.png">`;
+    mainImage.innerHTML = `<img src="/landingpage-apple-/images/4.png">`;
 }
 function gold(){
     document.body.style.background = "black";
-    mainImage.innerHTML = `<img src="/images/0.png">`;
+    mainImage.innerHTML = `<img src="/landingpage-apple-/images/0.png">`;
 }
 function blue(){
     document.body.style.background = "#247ec8";
-    mainImage.innerHTML = `<img src="/images/1.png">`;
+    mainImage.innerHTML = `<img src="/landingpage-apple-/images/1.png">`;
 }
 function black(){
     document.body.style.background = "#1e1e1e";
-    mainImage.innerHTML = `<img src="/images/2.png">`;
+    mainImage.innerHTML = `<img src="/landingpage-apple-/images/2.png">`;
 }
 function rose(){
     document.body.style.background = "#c79b53";
-    mainImage.innerHTML = `<img src="/images/3.png">`;
+    mainImage.innerHTML = `<img src="/landingpage-apple-/images/3.png">`;
 }
 let tlinks = document.querySelector('.t-links');
 let menuImage = document.querySelector('.hide');
